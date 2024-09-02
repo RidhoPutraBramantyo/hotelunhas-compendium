5a8bdcb5f7d2a251377cba4812d82be40dcb677f
RidhoPutraBramantyo <elm4c99@gmail.com>
2024-09-02 14:09:05 +0800

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Mengaktifkan dukungan trailing slash jika perlu
+  trailingSlash: true,
+
+  // Menambahkan konfigurasi lain jika diperlukan
+  // Misalnya, menambahkan basePath jika menggunakan subdirektori
+  // basePath: '/subpath',
+
+  // Atur opsi lainnya sesuai kebutuhan proyek Anda
+  // Misalnya, Anda dapat mengaktifkan kompresi Gzip atau Brotli di sini
+
+  // Tentukan direktori output jika diperlukan (biasanya default adalah 'out')
+  // outDir: 'output-directory-name'
+};
 
 export default nextConfig;
